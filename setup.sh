bb33437513d16bffe432fc9eedeab0ec8cc57c26
Sergey Samsonau <ssamsonau@users.noreply.github.com>
2019-11-24 18:13:03 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ export GOOGLE_APPLICATION_CREDENTIALS=/accounts/key.json
 [ -f ${GOOGLE_APPLICATION_CREDENTIALS} ] && echo "Credentials exist." || echo "Credentials does not exist."
 
 echo "Running fuse, bucket is ${BUCKET}"
-gcsfuse -o nonempty,allow_other ${BUCKET} /exports
+gcsfuse -o nonempty,allow_other --file-mode=777 --dir-mode=777 ${BUCKET} /exports
 
 function start()
 {
